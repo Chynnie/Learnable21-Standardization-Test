7db20c3a827da8a72b38c03dbde1e77663966bc6
Chynnie <33658059+Chynnie@users.noreply.github.com>
2021-12-08 14:41:44 +0100

    NavBar Updated Commit
    NavBar Updated Commit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,107 +1,48 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Button from './Button';
-import Dropdown from './Dropdown';
-import './NavBar.css'
-import Group from './Group.svg';
+import Group from './Group.svg'
 import Group1 from './Group1.svg';
 import Menu from './menu-outline.svg';
 import Cross from './cross.svg';
+import './NavBar.css'
 
-function NavBar() {
-  const [click, setClick] = useState(true);
-  const [dropdown, setDropdown] = useState(false);
+const NavBar = () => {
 
-  const handleClick = () => setClick(!click);
-  const mobileMenu = () => setClick(false);
-
-  const mouseEnter = () => {
-    if (window.innerWidth < 768) {
-      setDropdown(false);
-    } else {
-      setDropdown(true);
-    }
-  };
-
-  const mouseLeave = () => {
-    if (window.innerWidth < 768) {
-      setDropdown(false);
-    } else {
-      setDropdown(false);
-    }
-  };
-
+  const [mobile, setMobile] = useState(false);
   return (
-    <div>
+    <nav className='navbar'>
+      
+      <img
+        src={Group}
+        alt='logo'
+        className='desktop-logo'
+      />
 
-      <nav className='navbar'>
-        {/* <div> */}
-          <Link to='/'>
-            <img 
-              src={Group}
-              className='logo'
-              alt='logo'
-            />
-          </Link>
+      <img 
+        src={Group1}
+        alt='logo'
+        className='mobile-logo'
+      />
 
-          {/* Mobile Logo */}
-          <Link to='/'>
-            <img 
-              src={Group1} 
-              className='mobile-logo'
-              alt="logo" 
-            />
-          </Link>
-        {/* </div> */}
-
-        <div>
-          <img 
-            src={click ? Menu : Cross} 
-            className='menu-outline'
-            alt="menu" 
-            onClick={handleClick}
-            onMouseEnter={mouseEnter}
-            onMouseLeave={mouseLeave}
-          />
-          {dropdown && <Dropdown />}
-        </div>
-
-        {/* <div> */}
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-            <Link to='/' className='nav-links' onClick={mobileMenu}>
-              Features
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link to='/' className='nav-links' onClick={mobileMenu}>
-              About
-            </Link>
-          </li>
+      <div className='nav-mobile'>
+        <ul 
+          className={mobile ? 'nav-links-mobile' : 'nav-links'}
+          onClick={() => setMobile(false)}  
+        >
+          <li>Features</li>
+          <li>About</li>
         </ul>
-        {/* </div> */}
-        <div>
-          <Button />
-          {/* <button className='signup-button'>Sign up</button>
-          <button className='signin-button'>Sign in</button> */}
-        </div>
-        
-        {/* Menu Bar */}
-        {/* <img 
-          src={click ? Menu : Cross} 
-          className='menu-outline'
-          alt="menu" 
-          onClick={handleClick}
-          onMouseEnter={mouseEnter}
-          onMouseLeave={mouseLeave}
-        />
-        {dropdown && <Dropdown />} */}
-        
-      </nav>
 
-      
-      
-    </div>
+        <button className={mobile ? 'sign-up-mobile' : 'sign-up'}>Sign Up</button>
+        <button className={mobile ? 'sign-in-mobile' : 'sign-in'}>Sign In</button>
+      </div>
+
+      <img
+        src={ mobile ? ([Cross]) : ([Menu]) }
+        alt='menu'
+        className='mobile-menu'
+        onClick={() => setMobile(!mobile)} 
+      />
+    </nav>
   )
 }
 
